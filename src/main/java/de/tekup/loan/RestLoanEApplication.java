0f816113f0409a54ebbf3c35fc48c8577a4feea4
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-04-27 09:48:22 +0100

    rest api

---
+++ src/main/java/de/tekup/loan/RestLoanEApplication.java
@@ -0,0 +1,13 @@
+package de.tekup.loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestLoanEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestLoanEApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/tekup/loan/RestLoanEApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tekup.loan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestLoanEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
